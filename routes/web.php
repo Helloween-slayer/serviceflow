9ba65f52a7c7eb3a00aa93a934f8341d3e58b6b7
unknown <privalovov4@gmail.com>
2026-08-07 17:49:51 +0300

    Add Redis

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,7 @@ use Inertia\Inertia;
 
 // ========== ПУБЛІЧНІ ==========
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('orders.index');
 });
 
 // ========== ПУБЛІЧНИЙ ПРОФІЛЬ ВОРКЕРА (доступен всем) ==========
