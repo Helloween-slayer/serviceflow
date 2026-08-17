1218aa63b39ef803ed0c4c6266bb20a77c991164
unknown <privalovov4@gmail.com>
2026-08-17 20:11:58 +0300

    Remove failing example tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        $this->app->make(\Illuminate\Foundation\Vite::class)->prefetch(concurrency: 3);
 
         if (app()->environment('production')) {
             URL::forceScheme('https');
